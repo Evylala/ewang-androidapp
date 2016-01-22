e43383c80ad6ca8f126933b5a74dbf5ca74829f7
Evy Ily <Evy Ily>
2016-01-22 15:43:44 +0100

    Initial commit

---
+++ app/src/main/java/android/fil/emn/ewang/LibraryActivity.java
@@ -0,0 +1,14 @@
+package android.fil.emn.ewang;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by evyil on 22/01/2016.
+ */
+public class LibraryActivity {
+}
